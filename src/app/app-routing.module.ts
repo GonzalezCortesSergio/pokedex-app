5c81e454239b3ca702818e2f6cffd72ba2f66a4e
Patty <alonso.aypat22@triana.salesianos.edu>
2024-10-24 13:49:35 +0200

    bu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { MtListComponent } from './components/mt-list/mt-list.component';
 
 const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
   {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  {path: 'pokemons', component: PokemonListComponent},
+  {path: 'items', component: ItemListComponent},
+  {path: 'mts', component: MtListComponent},
   //{ path: '**', component: PageNotFoundComponent },
 ];
 
