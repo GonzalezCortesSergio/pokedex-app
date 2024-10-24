521a3d43c06f53c5d153eda7a2af2745cc2fa82a
Sergio González <gonzalez.coser24@triana.salesianos.edu>
2024-10-24 13:22:08 +0200

    Jiji

--- src/app/components/mt-modal/mt-modal.component.ts
+++ src/app/components/mt-modal/mt-modal.component.ts
@@ -19,4 +19,36 @@ capitalizeFirstLetter(string: string) {
 
   @Input()
   mt!:Mt;
+
+  getTypeColor(typeName: string): string {
+
+    let background = "background-color: ";
+
+    interface Color {
+
+      [index: string]: string;
+    }
+
+    let colores = {
+      normal: "#c6c1b9",
+      bug: "#8c980f",
+      dark: "#3e2d23",
+      dragon: "#6f5dc7",
+      electric: "#f9bc1a",
+      fighting: "#80351a",
+      fire: "#f0400a",
+      flying: "#94a4ee",
+      ghost: "#5d60aa",
+      grass: "#72c235",
+      ground: "#d1b154",
+      ice: "#76d6f4",
+      poison: "#934393",
+      psychic: "#ee4881",
+      rock: "#b6a257",
+      steel: "#b5b4c1",
+      water: "#2989e7"
+    } as Color
+
+    return `${background}${colores[typeName]}`
+  }
 }
